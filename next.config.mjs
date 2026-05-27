3cbc4f7949ac94a712f4d7ca0fada074d9dbc410
Atharva Kadam <atharvakadam751@gmail.com>
2026-05-27 12:12:42 +0530

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080';
+
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
@@ -6,14 +8,15 @@ const nextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:8080/api/v1/:path*',
+        destination: `${backendUrl}/api/v1/:path*`,
       },
       {
         source: '/user/:path*',
-        destination: 'http://localhost:8080/user/:path*',
+        destination: `${backendUrl}/user/:path*`,
       },
     ];
   },
 };
 
 export default nextConfig;
+
